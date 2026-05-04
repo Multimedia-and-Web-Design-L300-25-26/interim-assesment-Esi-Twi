d9488b8b7dd082c5c74b17fd034082513a4478a4
Esi-Twi <esitwi8@gmail.com>
2026-05-04 21:07:40 +0000

    update on profile II

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -49,8 +49,8 @@ exports.login = async (req, res) => {
 
         res.cookie('token', token, {
             httpOnly: true,
-            secure: false,
-            sameSite: 'strict',
+            secure: true,
+            sameSite: 'none',
         });
 
         res.json({
